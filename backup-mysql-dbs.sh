acb7345fb7f1cb22804f9a3e6ed62c14e7894601
miguelwill <31906851+miguelwill@users.noreply.github.com>
2019-06-21 18:20:16 -0400

    backup mysql multiples db
    respaldo todas las db mysql en archivos bzip2 separados

---
+++ backup-mysql-dbs.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+DIR=/var/backups/db
+DATE=`date +%H`
+PASS="password-root-o-similar"
+EXCLUDE="Database|information_schema|performance_schema"
+#obtiene lista de bases de datos
+DB=`echo "show databases;" | mysql -uroot -p$PASS|egrep -v $EXCLUDE`
+
+DIAS=10
+NICE="nice -n 10 ionice -c2 "
+mkdir -p /var/backups/db -m777
+cd $DIR
+
+#limpieza de mas de 10 dias
+#find -type f -name  "*.bz2" -mtime +$DIAS -delete
+
+for i in $DB;
+do
+        FILE=$i-$DATE.sql
+        $NICE        mysqldump  --triggers  -K -R -uroot -p$PASS $i | $NICE bzip2 -f > $FILE.bz2
+        sleep 1
+done
